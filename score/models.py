98ccfb7382e0d7d1ef75c104483f6a55e9f6be80
Punit Sharma <punitcs0067@gmail.com>
2024-03-19 14:42:12 +0530

    PS 1 modified admin side look

--- score/models.py
+++ score/models.py
@@ -8,6 +8,10 @@ class Score(models.Model):
     def __str__(self) -> str:
         return self.name
 
+#set default field of order in which it will show 
+    class Meta:
+        ordering = ['name']
+
 # Score(name='Punit', marks=90).save()
 # Score(name='Raju', marks=95).save()
 # Score(name='Ramla', marks=85).save()
