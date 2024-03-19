1fe60a96bc30e2fd88e415d96e4341d5df84a3d5
Punit Sharma <punitcs0067@gmail.com>
2024-03-19 18:01:19 +0530

    Added Logic for CRUD in student and score

--- score/views.py
+++ score/views.py
@@ -1,13 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Score
+from .forms import ScoreForm
 # Create your views here.
+
+# def index(request):
+#     return HttpResponse("Hello wordls")
 context = {}
 def index(request):
-    # return HttpResponse("Hello wordls")
     scores = Score.objects.all()
+    form = ScoreForm()
     context['student_data'] = scores
     context['title'] = 'Home'
+
+    #handling form data
+    if request.method == 'POST':
+        if 'save' in request.POST:
+            std_id = request.POST.get('save')
+
+            if not std_id: #empty means add new row
+                form = form = ScoreForm(request.POST)
+            else:
+                student = Score.objects.get(id=std_id)
+                form = ScoreForm(request.POST, instance=student)    
+            form.save()
+            form = ScoreForm()
+        elif 'delete' in request.POST:
+            std_id = request.POST.get('delete')
+            student = Score.objects.get(id=std_id) 
+            student.delete()
+        elif 'edit' in request.POST:
+            std_id = request.POST.get('edit')
+            student = Score.objects.get(id=std_id) 
+            form = ScoreForm(instance=student)
+                   
+    context['form'] = form
     return render(request, 'index.html', context)    
 
 def about(request):
