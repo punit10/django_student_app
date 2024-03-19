98dec46eb4451abbd6d35c8df9f5afdd7386993d
Punit Sharma <punitcs0067@gmail.com>
2024-03-19 13:46:51 +0530

    Added two more pages and done template tagging

--- score/views.py
+++ score/views.py
@@ -6,3 +6,8 @@ def index(request):
     # return HttpResponse("Hello wordls")
     return render(request, 'index.html')    
 
+def about(request):
+    return render(request, 'about.html') 
+
+def contactus(request):
+    return render(request, 'contact.html') 
