5bea269d163145a5bee3e152941e2f2402b0ed03
Punit Sharma <punitcs0067@gmail.com>
2024-03-19 14:20:36 +0530

    Added model for name and marks

--- score/models.py
+++ score/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Score(models.Model):
+    name = models.CharField(max_length=40)
+    marks = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+# Score(name='Punit', marks=90).save()
+# Score(name='Raju', marks=95).save()
+# Score(name='Ramla', marks=85).save()
+# Score(name='Rashmi', marks=75).save()
